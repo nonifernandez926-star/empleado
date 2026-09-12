8b268cf8f6d61cec823aacf415bd03190fdd1c60
nonifernandez926-star <nonifernandez926@gmail.com>
2026-09-12 12:06:39 -0300

    Delete backend/config/mercadopago.js

--- backend/config/mercadopago.js
+++
@@ -1,7 +0,0 @@
-const { MercadoPagoConfig, Preference, Payment } = require('mercadopago');
-
-const client = new MercadoPagoConfig({
-  accessToken: process.env.MP_ACCESS_TOKEN,
-});
-
-module.exports = { client, Preference, Payment };
